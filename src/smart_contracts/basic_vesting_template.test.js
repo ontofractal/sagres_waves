e34921ef15d55bdbbc3b066e0c92fa54deeb16f1
ontofractal <ontofractal@protonmail.com>
2018-07-22 18:53:33 +0100

    add an example of generated vesting smart contract

--- src/smart_contracts/basic_vesting_template.test.js
+++ src/smart_contracts/basic_vesting_template.test.js
@@ -1,33 +1,10 @@
-Sagres Vesting contracts for WAVES blockchain
+import {createContractFromTemplate} from "./basic_vesting_template";
+import {testContractConfig} from "./test_creds";
 
-# Sagres?
+it("renders without crashing", () => {
+  const result = createContractFromTemplate(testContractConfig)
 
-![](https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Leuchtturm_Algarve_20060422.jpg/640px-Leuchtturm_Algarve_20060422.jpg)
-
-Sagres is a beautiful place in Portugal, where steep cliffs are shaped and eroded by waves. As a fan of puns, I find Sagres to be a fitting name for vesting contracts for WAVES blockchain.
-
-# Why?
-
-Vesting is a tool that can be used to align incentives of project co-founders, employees, consultants and investors.
-
-
-# Features
-
-Sagres includes a vesting smart contract template that accepts a set of parameters and generates a RIDE smart contract that is designed to be attached to individual accounts of token holders.
-
-| Parameter | Description |
-| --- | ---- |
-| startingDate | A starting date for vesting period | 
-| originalAssetAmount | A starting amount of tokens for the given account |
-| assetId | asset ID |
-| ownerPk | account owner Public Key |
-| maxVestingPeriodWeeks | vesting period in weeks |
-| cliffPeriodWeeks | cliff period in weeks |
-
-
-# Example of generated smart contract 
-
-```
+  const testResult = `
   let startingUnixTimestampMsec = 1532214000000 
   let originalAssetAmount = 10000 
   let assetId = base58'EAhaYpFg9a3833aTgAxBBfLEKhP8yZkZ9qZpXDjBXUCg'
@@ -57,5 +34,6 @@ Sagres includes a vesting smart contract template that accepts a set of paramete
     case burnTx:BurnTransaction => true
     case _ => false
   }
-
-```
+`
+  expect(result).toEqual(testResult)
+});
