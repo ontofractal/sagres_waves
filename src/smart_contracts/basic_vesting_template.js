be4bbb4fc63a91b30a1bb971dabd6fea03d07dc4
ontofractal <ontofractal@protonmail.com>
2018-07-22 15:04:49 +0100

    improve code readability

--- src/smart_contracts/basic_vesting_template.js
+++ src/smart_contracts/basic_vesting_template.js
@@ -6,20 +6,20 @@ let ownerPk = base58'${config.ownerPk}'
 
 match tx {
   case tx:TransferTransaction =>
+    let txSenderIsOwner = ownerPk == tx.senderPk
+  
+    let vestingPeriodEnded = if elapsedWeeks > ${config.maxVestingPeriodWeeks} then true else false
+  
     let elapsedMsec = tx.timestamp - startingUnixTimestampMsec
     let elapsedWeeks = elapsedMsec / 7 * 86400 * 1000 
-    let vestingPeriodEnded = if elapsedWeeks > ${config.maxVestingPeriodWeeks} then true else false
-
-    let txSenderAddress = addressFromPublicKey(tx.senderPk)
-
+    
     let currentBalance = accountAssetBalance(txSenderAddress, assetId)
+    let remainingBalanceAfterTx = currentBalance - tx.amount
 
     let minRequiredAtTxTimestamp = if elapsedWeeks <= ${config.minCliffWeeks} then startingAssetAmount
         else startingAssetAmount * (52 - elapsedWeeks / 52 - ${config.minCliffWeeks})
 
-    let vestingConditionsMatch = (currentBalance - tx.amount) >= minRequiredAtTxTimestamp
-    
-    let txSenderIsOwner = ownerPk == tx.senderPk
+    let vestingConditionsMatch = remainingBalanceAfterTx >= minRequiredAtTxTimestamp
 
     sigVerify(tx.bodyBytes, tx.proofs[0], tx.senderPk) && txSenderIsOwner && (vestingPeriodEnded || vestingConditionsMatch)
 
